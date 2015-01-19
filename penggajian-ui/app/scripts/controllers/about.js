f5be3dc508f0e1fe630f36edc867ec967cb105dc
tedimulyadi <tedimulyadi92@gmail.com>
2015-01-19 13:33:20 +0700

    initial commit

---
+++ penggajian-ui/app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name penggajianUiApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the penggajianUiApp
+ */
+angular.module('penggajianUiApp')
+  .controller('AboutCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });

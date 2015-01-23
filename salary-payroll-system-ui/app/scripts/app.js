2f5c48e3b2796305ca73f73bec65d3ef78357242
tedimulyadi <tedimulyadi92@gmail.com>
2015-01-23 19:28:42 +0700

    edit logo

--- salary-payroll-system-ui/app/scripts/app.js
+++ salary-payroll-system-ui/app/scripts/app.js
@@ -32,8 +32,36 @@ angular.module('ewalletAdminApp', [
           templateUrl: 'views/merchant.html',
           controller: ''
         })
-            .when('/main/jabatan', {
+        .when('/main/jabatan', {
           templateUrl: 'views/jabatan.html',
+          controller: ''
+        })
+        .when('/main/bank', {
+          templateUrl: 'views/bank.html',
+          controller: ''
+        })
+         .when('/main/absensi', {
+          templateUrl: 'views/absensi.html',
+          controller: ''
+        })
+           .when('/main/cuti', {
+          templateUrl: 'views/cuti.html',
+          controller: ''
+        })
+         .when('/main/lembur', {
+          templateUrl: 'views/lembur.html',
+          controller: ''
+        })
+        .when('/main/pinjaman', {
+          templateUrl: 'views/pinjaman.html',
+          controller: ''
+        })
+        .when('/main/potongan', {
+          templateUrl: 'views/potongan.html',
+          controller: ''
+        })
+        .when('/main/gaji', {
+          templateUrl: 'views/gaji.html',
           controller: ''
         })
         .otherwise({
